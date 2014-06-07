3d9f86f3ec626fe742a06a4a62a51a19fca987cc
Ian Kallen <ian@klout.com>
2014-06-06 17:54:45 -0700

    fixed the startup script generation

--- project/Build.scala
+++ project/Build.scala
@@ -5,8 +5,19 @@ import com.typesafe.sbt.SbtStartScript
 
 object ApplicationBuild extends Build {
 
+  val orgName         = "potatoheads"
   val appName         = "IncidentResponseManager"
   val appVersion      = "1.0-SNAPSHOT"
+  val scalaVers       = "2.10.4"
+  val mainClassname   = "play.core.server.NettyServer"
+
+  val appSettings = playScalaSettings ++ Seq(
+      organization := orgName,
+      name         := appName,
+      version      := appVersion,
+      scalaVersion := scalaVers,
+      mainClass    := Some(mainClassname)
+  )
 
   val appDependencies = Seq(
     // Add your project dependencies here,
@@ -17,7 +28,7 @@ object ApplicationBuild extends Build {
     "org.apache.commons" % "commons-email" % "1.3"
   )
 
-  val allSettings = Defaults.defaultSettings ++ SbtStartScript.startScriptForClassesSettings
+  val allSettings = appSettings ++ SbtStartScript.startScriptForClassesSettings
 
   val main = play.Project(appName, appVersion, appDependencies, settings=allSettings).settings(
       // define the statements initially evaluated when entering 'console',
